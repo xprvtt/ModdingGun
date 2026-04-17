995e8a38fcde10279cdd43f0e0b2430da2ad597f
xprvtt <142792355+xprvtt@users.noreply.github.com>
2026-04-17 17:20:56 +0500

    рефакторинг

--- ModdingGun/WstringToString.cpp
+++ ModdingGun/WstringToString.cpp
@@ -4,9 +4,9 @@ string wstringToString(const wstring& wstr)
 {
     if (wstr.empty()) return {};
 
-    int size_needed = WideCharToMultiByte(CP_UTF8, 0, wstr.data(), (int)wstr.size(), nullptr, 0, nullptr, nullptr);
-    string strTo(size_needed, 0);
-    WideCharToMultiByte(CP_UTF8, 0, wstr.data(), (int)wstr.size(), &strTo[0], size_needed, nullptr, nullptr);
+    int sizeNeeded = WideCharToMultiByte(CP_UTF8, 0, wstr.data(), (int)wstr.size(), nullptr, 0, nullptr, nullptr);
+    string strTo(sizeNeeded, 0);
+    WideCharToMultiByte(CP_UTF8, 0, wstr.data(), (int)wstr.size(), &strTo[0], sizeNeeded, nullptr, nullptr);
 
     return strTo;
 }
